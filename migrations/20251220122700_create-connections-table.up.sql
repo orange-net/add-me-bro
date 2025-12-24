2d80bf567b7caa71b400ae358044f0a7bbefcef8
Tahmid Sadik <tahmidsadik112@gmail.com>
2025-12-24 10:43:07 +0100

    feat(connections): optimize connection storing for efficient querying and fetching of a connection

--- migrations/20251220122700_create-connections-table.up.sql
+++ migrations/20251220122700_create-connections-table.up.sql
@@ -4,6 +4,12 @@ CREATE TABLE connections (
   id SERIAL PRIMARY KEY,
   initiator_uid int NOT NULL REFERENCES users(user_id),
   recipient_uid  int NOT NULL REFERENCES users(user_id),
+  -- ✅ canonical, order-independent, guaranteed-unique key
+  connection_key TEXT GENERATED ALWAYS AS (
+    LEAST(initiator_uid, recipient_uid)::text
+    || '::' ||
+    GREATEST(initiator_uid, recipient_uid)::text
+  ) STORED,
   status varchar(50) NOT NULL DEFAULT 'Pending',
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
